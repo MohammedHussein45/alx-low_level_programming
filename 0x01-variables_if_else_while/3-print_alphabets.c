134fbee382ee7f4d820c991bcb2c03bf89ff5d90
MohammedHussein45 <moahmmedhussein7@gmail.com>
2023-03-17 12:25:58 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i = 'A';
+for (; i <= 'Z'; i++)
+{
+putchar(tolower(i));
+}
+for (; i <= 'Z'; i++)
+putchar(toupper(i))
+printf("\n");
+return (0);
+}
