cf8d5c5ce5ae77f936dda0f346eb30a48619044e
MohammedHussein45 <moahmmedhussein7@gmail.com>
2023-03-17 13:13:03 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+if (i != 57)
+{
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
