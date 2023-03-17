81cd4e7a51ee17732404c54bd8399c070f3899ad
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-03-17 22:38:45 +0200

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,19 +5,19 @@
 */
 int main(void)
 {
-int i, m, n;
-for (i = 48; i <= 58; i++)
+int n, m, l;
+for (n = 48; n <= 58; i++)
 {
 for (m = 49; m <= 58; m++)
 {
-for (n = 50; n <= 58; n++)
+for (l = 50; l <= 58; n++)
 {
-if (m > i && n > m)
+if (l > m && m > n)
 {
-putchar(i);
-putchar(m);
 putchar(n);
-if (i != 55 || m != 56)
+putchar(m);
+putchar(l);
+if (n != 55 || m != 56)
 {
 putchar(',');
 putchar(' ');
