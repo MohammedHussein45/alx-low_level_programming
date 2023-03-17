50d529978338989c09b791b32a0b56c7d403981b
MohammedHussein45 <moahmmedhussein7@gmail.com>
2023-03-17 13:05:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+for (i = 'a'; i < 'g'; i++)
+{
+putchar(i);
+}
+putchar("\n");
+return (0);
+}
