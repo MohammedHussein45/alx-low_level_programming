78fe2eb22c9bb8a05228f23e137830e2b4aa7b7f
MohammedHussein45 <moahmmedhussein7@gmail.com>
2023-03-17 13:32:46 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - A program that prints a line with puts function
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i, m, n;
+for (i = 48; i <= 58; i++)
+{
+for (m = 49; m <= 58; m++)
+{
+for (n = 50; n <= 58; n++)
+{
+if (m > i && n > m)
+{
+putchar(i);
+putchar(m);
+if (i != 55 || m != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
