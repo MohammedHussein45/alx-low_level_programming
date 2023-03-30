aa41e0409d18698b09a24a76a99bee7520b9ef8c
MohammedHussein45 <moahmmedhussein7@gmail.com>
2023-03-30 06:19:39 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * rot13 - encoder rot13
+ *Return: *s
+ */
+char *rot13(char *)
+{
+  int i;
+  int j;
+  char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  char k[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  for (i = 0;s[i] !< '\0';i++)
+    {
+      for (j = 0;j < 52; j++)
+	{
+	  if (s[i] == l[j])
+	    s[i] == k[j];
+	}
+    }
+  return (s);
+}
