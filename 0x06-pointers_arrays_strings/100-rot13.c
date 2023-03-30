43b9948793d655b9b054e9c055f82a318ab79d86
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-03-30 15:22:51 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,17 +6,17 @@
  */
 char *rot13(char *)
 {
-  int i;
-  int j;
-  char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char k[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-  for (i = 0;s[i] !< '\0';i++)
-    {
-      for (j = 0;j < 52; j++)
+	int i;
+	int j;
+	char l[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char k[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	  if (s[i] == l[j])
-	    s[i] == k[j];
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == l[j])
+				s[i] == k[j];
+		}
 	}
-    }
-  return (s);
+	return (s);
 }
