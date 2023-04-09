a75e6ee9c5287e05ec72c3db5723e4f064cccf3d
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-04-09 21:32:14 +0200

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
  * _atoi - convert a string into an integer.
- *
  * @s: the string to use.
  *
- * Return: integer.
+ * Return: 0 or 1
  */
 int _atoi(char *s)
 {
-int sign = 1, i = 0;
-unsigned int res = 0;
+int x = 1, i = 0;
+unsigned int y = 0;
 
 while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 {
 if (s[i] == '-')
-sign *= -1;
+x *= -1;
 i++;
 }
 while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 {
-res = (res * 10) + (s[i] - '0');
+y = (y * 10) + (s[i] - '0');
 i++;
 }
-res *= sign;
-return (res);
+y *= x;
+return (y);
 }
