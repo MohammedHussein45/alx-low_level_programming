430e38d4d29d6ee07f4a0aac649b87e1c09571ae
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-04-09 21:31:51 +0200

    Update 1-strncat.c

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,29 +1,24 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: dest
+ * _strncat - concatenates two strings
+ * @src : string to be copied
+ * @dest : string to be placed at
+ * @n : number of words to be concatenated
+ *
+ * Return: 0 or 1
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
-
-
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
-	return (dest);
+int x = 0;
+int y;
+while (dest[x] != '\0')
+{
+x++;
+}
+for (y = 0; y < n && src[y] != '\0'; ++y, ++x)
+{
+dest[x] = src[y];
+}
+dest[x] = '\0';
+return (dest);
 }
