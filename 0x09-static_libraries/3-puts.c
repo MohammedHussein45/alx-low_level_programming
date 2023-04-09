1d69a24d481f8c3416b3f2ee9ce42876e6704cef
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-04-09 21:34:08 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
 /**
- * main - Entry point
+ * _puts - prints a string
+ * @str : string to be printed
  *
- * Return: Always 0 (Success)
+ * Return: 0 or 1
  */
-int main(void)
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+while (*str != 0)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }
