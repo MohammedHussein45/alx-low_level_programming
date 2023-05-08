0e74a415153365c89348be42453fdfa25d5064f7
mohammed hussein <125441639+MohammedHussein45@users.noreply.github.com>
2023-05-08 13:12:59 +0300

    Create 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Read text file print to STDOUT
+ * @filename: text file
+ * @letters: number of letters to be read
+ * Return: number of bytes read or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *x;
+	ssize_t file;
+	ssize_t writefile;
+	ssize_t readfile;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	x = malloc(sizeof(char) * letters);
+	readfile = read(file, x, letters);
+	writefile = write(STDOUT_FILENO, x, readfile);
+
+	free(x);
+	close(file);
+	return (writefile);
+}
